1b44193ba15a6a12f9e1b32cd6215aa8c793c7a3
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-05-01 12:09:31 -0300

    Empezando Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Inicio');
+
+    const themesOptions = document.querySelector('.themes__options');
+
+    themesOptions.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault()
+            themesOptions.click();
+        }
+    });
 });
